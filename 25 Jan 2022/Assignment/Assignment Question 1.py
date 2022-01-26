a03bcf17dba12ad2bbb0cd7592179f63ae58e234
Shourabhmishra2405 <98306440+Shourabhmishra2405@users.noreply.github.com>
2022-01-26 13:42:24 +0530

    Update Assignment Question 1.py

--- 25 Jan 2022/Assignment/Assignment Question 1.py
+++ 25 Jan 2022/Assignment/Assignment Question 1.py
@@ -1,6 +1,10 @@
-Maximum = [0]
+# Initialising and empty list
+Maximum = []
+# Iterating for n number of times according to user input
 for i in range(int(input())):
+    # Seperating out the inputs into operation and value to input
     nums = list(map(int, input().split()))
+    # Operations according to the inputs
     if nums[0] == 1:
         Maximum.append(max(nums[1], Maximum[-1]))
     elif nums[0] == 2:
