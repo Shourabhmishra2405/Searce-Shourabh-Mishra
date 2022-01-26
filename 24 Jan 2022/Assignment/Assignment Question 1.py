66f67518395b5ae2c1b0066a2dcf8b0280b2d1d2
Shourabhmishra2405 <98306440+Shourabhmishra2405@users.noreply.github.com>
2022-01-26 13:21:01 +0530

    Update Assignment Question 1.py

--- 24 Jan 2022/Assignment/Assignment Question 1.py
+++ 24 Jan 2022/Assignment/Assignment Question 1.py
@@ -1,3 +1,4 @@
+# Defining the function to reverse the elements present in the list P with the starting index start and ending index end.
 def reverseList(P, start, end):
     while start < end:
         P[start], P[end] = P[end], P[start]
@@ -5,6 +6,9 @@ def reverseList(P, start, end):
         end -= 1
     for x in range(len(P)):
         print(P[x],end = ' ')
+# Taking inputs from user
+# Input for number of integer to reverse.
 n = int(input('Enter Number of integers'))
+# Input for numbers to reverse
 arr = list(map(int, input().split()))
 reverseList(arr,0,n-1)
